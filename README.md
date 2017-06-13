85518f14157e5159860f8c827f87647a1e7a6522
Jon Nilsson <jon.h.nilsson@gmail.com>
2017-06-13 22:41:33 +0200

    Add check for object

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # intensify.js
-
 Make it true, make it false!
+Intensifies it's argument:
+Makes Truthy stuff true, and Falsy stuff False
 
 Todo: Add casual mode, and intense mode (default)
+
+**Install**
+```bash
+$ npm i -S intensify
+```
+```js
+import {intensify} from 'intensify';
+
+intensify({})
+=> false
+intensify('')
+=> false
+intensify(undefined)
+=> false
+
+//and so on! Very good 
+```
--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-export default intensify = (weakValue) => {
-    if(weakValue == '') {
-        return false
-    } else if(weakValue == {}) {
-        return false;
-    } else if(weakValue == undefined) {
-        return false;
-    } else if(weakValue == []) {
-        return false;
-    } else if(weakValue == 0) {
-        return false;
-    } else if(weakValue == null) {
-        return false;
-    } else if(weakValue == false) {
-        return false;
-    } else if(typeof weakValue == 'function') { 
-        return this.intensify(weakValue())
-    } else if(!weakValue) {
-        return false;
-    } else {
-        return true;
-    }
-
-}
+export const intensify = (weakValue) => {
+  if(weakValue == '') {
+    return false;
+  } else if(typeof weakValue == 'object') {
+    return !Object.keys(weakValue ).length === 0 && weakValue .constructor === Object
+  } else if(weakValue == undefined) {
+    return false;
+  } else if(weakValue == []) {
+    return false;
+  } else if(weakValue == 0) {
+    return false;
+  } else if(weakValue == null) {
+    return false;
+  } else if(weakValue == false) {
+    return false;
+  } else if(typeof weakValue == 'function') { 
+    return this.intensify(weakValue());
+  } else if(!weakValue) {
+    return false;
+  } else {
+    return true;
+  }
+};
